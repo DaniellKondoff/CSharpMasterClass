a20a34484fb6214a5aefc157ed9e0961d4c56148
DKondoff <danielkondov91gmail.com>
2019-11-08 16:57:58 +0200

    Events

---
+++ Delegates/Events/Events/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cat = new Cat
+            {
+                Id = 1,
+                Name = "Cat Name",
+                Health = 100
+            };
+
+            cat.OnHealthChanged += CatOnHealthChanged;
+            cat.OnHealthChanged += CatIsRemoved;
+
+            cat.Health = 200;
+        }
+
+        private static void CatOnHealthChanged(object sender, int health)
+        {
+            var cat = sender as Cat;
+
+            Console.WriteLine($"{cat.Name} has new Health: {health}");
+        }
+
+        private static void CatIsRemoved(object sender, int health)
+        {
+            var cat = sender as Cat;
+
+            Console.WriteLine($"{cat.Name} has been removed");
+        }
+    }
+}
