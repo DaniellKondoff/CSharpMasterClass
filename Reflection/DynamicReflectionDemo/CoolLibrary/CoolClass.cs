82ca12e91e0414056f1004d4631c62685fa6afb2
Daniel Kondow <danielkondov91@gmail.com>
2019-11-12 09:57:33 +0200

    DynamicReflectionTrick

---
+++ Reflection/DynamicReflectionDemo/CoolLibrary/CoolClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CoolLibrary
+{
+    internal static class CoolClass
+    {
+        internal static string Name { get; set; }
+
+        internal static void CoolMethod()
+        {
+            Console.WriteLine(42);
+        }
+    }
+}
---
+++ Reflection/DynamicReflectionDemo/DynamicReflectionDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace DynamicReflectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var assembly = Assembly.Load(new AssemblyName("CoolLibrary"));
+
+            var type = assembly.GetTypes().FirstOrDefault(t => t.Name == "CoolClass");
+
+            //var methodInfo = type.GetMethod("CoolMethod");
+            //methodInfo.Invoke(null, new object[0]);
+
+
+            dynamic someCoolClass = new ExposedObject(type);
+
+            someCoolClass.CoolMethod();
+
+            someCoolClass.Name = "Test";
+
+            Console.WriteLine(someCoolClass.Name);
+        }
+    }
+}
